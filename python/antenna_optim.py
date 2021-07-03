6c1f6af81f1825acca44a1ed778ac6ecf8d0bb74
Syafiq Al Atiiq <syafiq.atiiq@gmail.com>
2021-07-03 12:39:24 +0200

    eigen function has different output between matlab vs python
    The initial procedure seems to work, but the result of eigen
    function seems to be different to what I've seen in my matlab.
    
    I will try to look for possibility to use eigen function from matlab
    and call it from python instead.

--- python/antenna_optim.py
+++ python/antenna_optim.py
@@ -3,7 +3,7 @@ import numpy as np
 
 f = scipy.io.loadmat('data.mat')
 Vin = f.get('Vin')
-Z0 = f.get('Z0')
+Z0 = np.matrix(f.get('Z0'))
 Zid = f.get('Zid')
 bas = f.get('bas')
 etaZ = f.get('etaZ')
@@ -11,7 +11,8 @@ evh = f.get('evh')
 k = f.get('k')
 kvh = f.get('kvh')
 meshp = f.get('meshp')
-
-print(Z0)
-print("===========")
-print(np.transpose(Z0))
+X0 = (Z0 - Z0.getH())/2j
+Rr = ((Z0 + Z0.getH())/2)*10000 # idk why it has to be multiplied with 10000
+Rs = 0.001
+V, D = np.linalg.eig(Rr)
+print(V)
